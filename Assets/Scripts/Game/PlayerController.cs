133871d9fe5566ab9e4e558bc4b0afbde47c58cf
vrtx <vortexshrimp@gmail.com>
2025-07-08 01:05:14 +0200

    Added Player & Camera controllers.

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    /// <summary>
+    /// This class is used to control the player in the game.
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Space War
 
 A dope space game bro.
+
+## Info
+
+Unity version `6000.0.42f1`.
